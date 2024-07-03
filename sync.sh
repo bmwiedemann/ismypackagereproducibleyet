6c9c8132fd40d321688cc90ab006827a1112a420
Bernhard M. Wiedemann <githubbmw@lsmod.de>
2024-07-03 13:41:31 +0200

    Make sync target a variable

--- sync.sh
+++ sync.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-rsync -a web/cgi/impryo.cgi root@vm12.zq1.de.:/usr/lib/cgi-bin/
-rsync -a web/css/main.css root@vm12.zq1.de.:/var/www/zq1/impryo/
-rsync -a cache/out/ root@vm12.zq1.de.:/usr/local/share/impryo/
+d=root@vm12.zq1.de.:
+rsync -a web/cgi/impryo.cgi $d/usr/lib/cgi-bin/
+rsync -a web/css/main.css $d/var/www/zq1/impryo/
+rsync -a cache/out/ $d/usr/local/share/impryo/
